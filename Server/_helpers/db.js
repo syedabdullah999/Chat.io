07e633b27efd92be20b6ad6e1de44d71e57529de
Syed Abdullah <syedabdullah992.com>
2023-06-25 23:44:04 +0500

    DeployOnVercel-1

--- Server/_helpers/db.js
+++ Server/_helpers/db.js
@@ -1,7 +1,8 @@
 const config = require('config.json');
 const mongoose = require('mongoose');
 const connectionOptions = { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false };
-mongoose.connect(process.env.MONGODB_URI || config.connectionString, connectionOptions);
+// mongoose.connect(process.env.MONGODB_URI || config.connectionString, connectionOptions);
+mongoose.connect("mongodb+srv://Syed:<password>@cluster1.8rjhedc.mongodb.net/?retryWrites=true&w=majority");
 mongoose.Promise = global.Promise;
 
 module.exports = {
