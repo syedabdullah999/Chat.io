328f80937d8c835de0c1cd327f2d90bf67a4b2ee
Syed Abdullah <syedabdullah992.com>
2023-06-26 03:01:13 +0500

    connectionOptionsupdated

--- Server/_helpers/db.js
+++ Server/_helpers/db.js
@@ -3,9 +3,7 @@ import mongoose from 'mongoose';
 const connectionOptions = { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false };
 // mongoose.connect(process.env.MONGODB_URI || config.connectionString, connectionOptions);
 const key = "mongodb+srv://Syed:Abcd@1234+@cluster1.8rjhedc.mongodb.net/users?retryWrites=true&w=majority"
-mongoose.connect(key,{
-    useNewUrlParser: true
-});
+mongoose.connect(key,connectionOptions);
 mongoose.Promise = global.Promise;
 
 module.exports = {
