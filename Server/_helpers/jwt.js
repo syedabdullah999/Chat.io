b775a32fa8a7349e27e649e0187aa63bfdd90412
Syed Abdullah <syedabdullah992.com>
2023-06-28 02:09:46 +0500

    UpdatingProxy

--- Server/_helpers/jwt.js
+++ Server/_helpers/jwt.js
@@ -10,6 +10,7 @@ function jwt() {
         path: [
             // public routes that don't require authentication
             '/',
+            '/users',
             '/users/authenticate',
             '/users/register',
             
